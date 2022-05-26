0e9270bf4a50de8fc1d88883f4a2009f11457b6b
Emilio-dot <saldanitasalinas@gmail.com>
2022-05-26 10:22:51 -0500

    primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import usuario from './usuario'
+
+
+function App() {
+    return ( <
+        div className = 'app' >
+        <
+        h1 > Holaxd < /h1>   < /
+        div >
+    );
+}
+
+export default App;
---
+++ src/usuario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function usuario(props) {
+    return ( <
+        h1 > Hola { props.nombre } < /h1>
+    );
+}
+
+export default usuario;
