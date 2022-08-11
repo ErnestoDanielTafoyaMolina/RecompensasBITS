c9dfba790dd519c7a66f3628d440086f267aabc7
Alvaro <200138@utags.edu.mx>
2022-08-11 17:50:20 -0500

    ya chsm el sprint

---
+++ precache-manifest.283fa4ecc1618cd5d1334c34ef4bdbda.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c9df26189d1fa0445d",
+    "url": "/static/css/main.74933578.chunk.css"
+  },
+  {
+    "revision": "22c9df26189d1fa0445d",
+    "url": "/static/js/main.22c9df26.chunk.js"
+  },
+  {
+    "revision": "e2faf49d932d60568032",
+    "url": "/static/css/1.72a08348.chunk.css"
+  },
+  {
+    "revision": "e2faf49d932d60568032",
+    "url": "/static/js/1.e2faf49d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70b4d16a14c25f1aad6ad77b4a9b8e4f",
+    "url": "/static/media/logo-bits.70b4d16a.png"
+  },
+  {
+    "revision": "2c121db3fb3030360edef74952f06c12",
+    "url": "/static/media/default.2c121db3.png"
+  },
+  {
+    "revision": "6468634d7642b247f4a6247b94d05553",
+    "url": "/static/media/left-arrow.6468634d.svg"
+  },
+  {
+    "revision": "279196e37844ff702af163035a67c609",
+    "url": "/static/media/right-arrow.279196e3.svg"
+  },
+  {
+    "revision": "1cf664ef403afee5aecdd7711a4b2281",
+    "url": "/static/media/error.1cf664ef.png"
+  },
+  {
+    "revision": "59e0a2da3f750061530dd5189525a654",
+    "url": "/index.html"
+  }
+];
